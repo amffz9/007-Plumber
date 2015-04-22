dd28d48a108db041141e0da17c4e38f43f0f2931
amffz9 <amffz9@mail.missouri.edu>
2015-04-22 12:51:15 -0500

    Mostly Scripting changes

--- Assets/_Scripts/Character.cs
+++ Assets/_Scripts/Character.cs
@@ -3,13 +3,58 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [Header("Character Movement Info")]
+    public bool facingRight = true;
+    public float forceMultiplier = 15f;
+    public float maxVelocity = 5f;
+    public float jumpStrength = 5f;
+
+    private float scaleX;
+    private Vector3 scale;
+
+    void Update()
+    {
+        if (facingRight == false)
+        {
+            
+            if (transform.localScale.x > 0)
+            {
+                switchX();
+            }  
+        }
+        else
+        {
+            if (transform.localScale.x < 0)
+            {
+                switchX();
+            }
+        }
+    }
+    public void switchX()
+    {
+        scaleX = transform.localScale.x;
+        scaleX *= -1;
+        scale = transform.localScale;
+        scale.x = scaleX;
+        transform.localScale = scale;
+    }
+    public void Jump()//use on GetKeyDown or equivilent
+    {
+        Debug.Log("Jump");
+        rigidbody.AddForce(Vector3.up * jumpStrength);
+    }
+    public bool OnGround()
+    {
+        Ray ray = new Ray(transform.position, transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -3,5 +3,34 @@ using System.Collections;
 
 public class Hero : Character {
 
-	
+    //Private variables
+    private float x = 0f;
+    
+    void FixedUpdate()
+    {
+        x = Input.GetAxis("Horizontal") * forceMultiplier;
+        //y = Input.GetAxis("Vertical") * forceMultiplier;
+        if (x < 0)
+        {
+            facingRight = false;
+
+        }
+        else if (x > 0)
+        {
+            facingRight = true;
+        }
+
+        rigidbody.AddForce(x, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();  
+        }
+
+        
+
+        
+
+
+    }
 }
