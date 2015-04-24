18468ea198642d94c8deb4cb65d49b646a26bc0b
amffz9 <amffz9@mail.missouri.edu>
2015-04-24 17:55:07 -0500

    Courtney's Stuff

--- Assets/_Scripts/GameFunctions/FollowCam.cs
+++ Assets/_Scripts/GameFunctions/FollowCam.cs
@@ -1,30 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FollowCam : MonoBehaviour
-{
-
-    static public FollowCam S;
-    //set by inspector
-    [Header("Camera Follow Settings")]
-    public float easing = .01f;
-    public GameObject poi;//the point of interest
-    private Vector3 temp;
-
-    void Awake()
-    {
-        S = this;
-        
-    }
-    void FixedUpdate()
-    {
-        if (poi == null)
-        {
-            return;
-        }
-        temp = poi.transform.position;
-        temp.z = this.camera.transform.position.z;
-        this.camera.transform.position = 
-            Vector3.Lerp(this.camera.transform.position, temp,easing);
-    }
+public class followCam : MonoBehaviour {
+	static public followCam S;
+	public float easing = .05f;
+	public Vector2 minXY;
+	public bool _________________;
+	public GameObject poi;
+	public float camZ;
+	
+	void Awake(){
+		S = this;
+		camZ = this.transform.position.z;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 destination;
+		destination = this.transform.position; 
+		destination.x += 1f; 
+		destination = Vector3.Lerp (transform.position, destination, easing);
+		destination.z = camZ;
+		transform.position = destination;
+		//this.camera.orthographicSize = destination.y + 10;
+	}
+	
 }
