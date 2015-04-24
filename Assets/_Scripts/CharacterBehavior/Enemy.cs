07a9974bfb344a49dc3ced5e975acd26effc9304
Emmanuel Pottipadu <epottipadu@gmail.com>
2015-04-24 17:15:31 -0500

    Added ninja man
    Edited Enemy class to make the ninja move around

--- Assets/_Scripts/CharacterBehavior/Enemy.cs
+++ Assets/_Scripts/CharacterBehavior/Enemy.cs
@@ -3,5 +3,34 @@ using System.Collections;
 
 public class Enemy : Character {
 
+    //Private variables
+    private float x = 0f;
+
+    public override void Move()
+    {
+        x = Input.GetAxis("Horizontal") * forceMultiplier;
+        //y = Input.GetAxis("Vertical") * forceMultiplier;
+        if (x < 0)
+        {
+            facingRight = false;
+
+        }
+        else if (x > 0)
+        {
+            facingRight = true;
+        }
+
+        rigidbody.AddForce(x, 0, 0);
+
+        if ((Input.GetAxis("Jump") != 0) && OnGround())
+        {
+            Jump();
+        }
+        if (Input.GetAxis("Fire1") != 0)
+        {
+            Shoot();
+        }
+
+    }
 	
 }
